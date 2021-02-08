4c0925e597695305ec21c6834ca5d20cb9dcf625
braceal <braceal@umich.edu>
2021-02-08 14:34:09 -0800

    residue preprocess

--- mdgraph/data/preprocess.py
+++ mdgraph/data/preprocess.py
@@ -1,8 +1,9 @@
 import MDAnalysis
-
+from sklearn import preprocessing
 
 def aminoacid_1hot(pdb_file):
     u = MDAnalysis.Universe(pdb_file)
-    residues = u.atoms.residues
-    print(residues)
-    return residues
+    resnames = [r.resname for r in u.atoms.residues]
+    le = preprocessing.LabelEncoder()
+    labels = le.fit_transform(resnames)
+    return resnames, labels
--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -6,5 +6,6 @@ TEST_DATA_PATH = Path(__file__).parent / "data/1FME-unfolded.pdb"
 
 
 def test_residue_list():
-    residues = aminoacid_1hot(TEST_DATA_PATH)
-    print(residues)
+    residues, labels = aminoacid_1hot(str(TEST_DATA_PATH))
+    assert len(residues) == 28
+    assert all(isinstance(r, str) for r in residues)
