cb33edacd8f7e0bd77152afbe705539e96f918d3
braceal <braceal@umich.edu>
2021-02-08 13:25:53 -0500

    data loader test

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -10,3 +10,12 @@ def test_shapes():
     sample = dataset[0]
     assert sample["X"].num_features == 5
     assert sample["X"].num_nodes == 28
+
+
+def test_dataloader():
+    from torch_geometric.data import DataLoader
+
+    dataset = ContactMapDataset(TEST_DATA_PATH, "contact_map", ["rmsd"], 5)
+    loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    assert len(loader) == len(dataset) // 32
