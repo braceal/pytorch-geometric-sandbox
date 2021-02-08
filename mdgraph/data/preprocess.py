56f228a9aa15a21633bd99355d4a658962c83f47
braceal <braceal@umich.edu>
2021-02-08 15:12:42 -0800

    onehot aa encoding

--- mdgraph/data/preprocess.py
+++ mdgraph/data/preprocess.py
@@ -1,9 +1,17 @@
 import MDAnalysis
+import numpy as np
 from sklearn import preprocessing
 
-def aminoacid_1hot(pdb_file):
+def aminoacid_int_encoding(pdb_file):
     u = MDAnalysis.Universe(pdb_file)
     resnames = [r.resname for r in u.atoms.residues]
     le = preprocessing.LabelEncoder()
     labels = le.fit_transform(resnames)
     return resnames, labels
+
+def aminoacid_int_to_onehot(labels):
+    total_aa = np.max(labels) + 1 
+    onehot = np.zeros((len(labels), total_aa))
+    for i, label in enumerate(labels):
+        onehot[i][label] = 1
+    return onehot
--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from mdgraph.data.preprocess import aminoacid_1hot
+import numpy as np
+from mdgraph.data.preprocess import aminoacid_int_encoding, aminoacid_int_to_onehot
 
 
 TEST_DATA_PATH = Path(__file__).parent / "data/1FME-unfolded.pdb"
 
 
-def test_residue_list():
-    residues, labels = aminoacid_1hot(str(TEST_DATA_PATH))
+def test_residue_onehot_encoding():
+    residues, labels = aminoacid_int_encoding(str(TEST_DATA_PATH))
     assert len(residues) == 28
     assert all(isinstance(r, str) for r in residues)
+    num_unique_aa = len(np.unique(labels))
+    onehot = aminoacid_int_to_onehot(labels)
+    assert onehot.shape == (len(labels), num_unique_aa)
