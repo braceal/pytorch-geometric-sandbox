c412fb1a0b2dcc512af519ff8974acddc9a1c3da
braceal <braceal@umich.edu>
2021-02-08 14:11:51 -0500

    added multiple graphs to training set

--- mdgraph/models/argva.py
+++ mdgraph/models/argva.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from mdgraph.dataset import ContactMapDataset
+from torch_geometric.data import DataLoader
 from torch_geometric.nn import GCNConv, ARGVA
 
 
@@ -34,18 +35,19 @@ class Discriminator(nn.Module):
 
 
 # Data
+num_features = 5
 path = Path(__file__).parent / "../../test/data/BBA-subset-100.h5"
-dataset = ContactMapDataset(path, "contact_map", ["rmsd"], 5)
-data = dataset[0]["X"]
+dataset = ContactMapDataset(path, "contact_map", ["rmsd"], num_features)
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 # Models
-encoder = Encoder(data.num_features, hidden_channels=32, out_channels=32)
+encoder = Encoder(num_features, hidden_channels=32, out_channels=32)
 discriminator = Discriminator(in_channels=32, hidden_channels=64, out_channels=32)
 model = ARGVA(encoder, discriminator)
 
 # Hardware
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model, data = model.to(device), data.to(device)
+model = model.to(device)
 
 # Optimizers
 discriminator_optimizer = torch.optim.Adam(discriminator.parameters(), lr=0.001)
@@ -53,23 +55,31 @@ encoder_optimizer = torch.optim.Adam(encoder.parameters(), lr=0.005)
 
 
 def train():
-    model.train()
-    encoder_optimizer.zero_grad()
-    z = model.encode(data.x, data.edge_index)
+    train_loss = 0.0
+    for sample in loader:
+        model.train()
+        encoder_optimizer.zero_grad()
+        data = sample["X"]
+        data = data.to(device)
+        z = model.encode(data.x, data.edge_index)
 
-    for _ in range(5):
-        discriminator.train()
-        discriminator_optimizer.zero_grad()
-        discriminator_loss = model.discriminator_loss(z)
-        discriminator_loss.backward()
-        discriminator_optimizer.step()
+        for _ in range(5):
+            discriminator.train()
+            discriminator_optimizer.zero_grad()
+            discriminator_loss = model.discriminator_loss(z)
+            discriminator_loss.backward()
+            discriminator_optimizer.step()
 
-    loss = model.recon_loss(z, data.edge_index)
-    loss = loss + (1 / data.num_nodes) * model.kl_loss()
-    loss.backward()
-    encoder_optimizer.step()
-    return loss
+        loss = model.recon_loss(z, data.edge_index)
+        loss = loss + (1 / data.num_nodes) * model.kl_loss()
+        loss.backward()
+        encoder_optimizer.step()
+        train_loss += loss.item()
+    train_loss /= len(loader)
+    return train_loss
 
+
+print(f"Traning on {len(loader)} examples")
 
 for epoch in range(1, 151):
     loss = train()
