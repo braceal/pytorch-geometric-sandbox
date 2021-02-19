6d5f71c0bfe115d49e4567139ff94ee55150da7e
braceal <braceal@umich.edu>
2021-02-19 11:31:21 -0500

    rm cahcing

--- mdgraph/models/gnn-lstm.py
+++ mdgraph/models/gnn-lstm.py
@@ -25,8 +25,8 @@ print("constant:", args.constant)
 class GCNEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(GCNEncoder, self).__init__()
-        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=False)
-        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=False)
+        self.conv1 = GCNConv(in_channels, 2 * out_channels)
+        self.conv2 = GCNConv(2 * out_channels, out_channels)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
@@ -36,9 +36,9 @@ class GCNEncoder(torch.nn.Module):
 class VariationalGCNEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(VariationalGCNEncoder, self).__init__()
-        self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=False)
-        self.conv_mu = GCNConv(2 * out_channels, out_channels, cached=False)
-        self.conv_logstd = GCNConv(2 * out_channels, out_channels, cached=False)
+        self.conv1 = GCNConv(in_channels, 2 * out_channels)
+        self.conv_mu = GCNConv(2 * out_channels, out_channels)
+        self.conv_logstd = GCNConv(2 * out_channels, out_channels)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index).relu()
@@ -48,7 +48,7 @@ class VariationalGCNEncoder(torch.nn.Module):
 class LinearEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(LinearEncoder, self).__init__()
-        self.conv = GCNConv(in_channels, out_channels, cached=False)
+        self.conv = GCNConv(in_channels, out_channels)
 
     def forward(self, x, edge_index):
         return self.conv(x, edge_index)
@@ -57,8 +57,8 @@ class LinearEncoder(torch.nn.Module):
 class VariationalLinearEncoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(VariationalLinearEncoder, self).__init__()
-        self.conv_mu = GCNConv(in_channels, out_channels, cached=False)
-        self.conv_logstd = GCNConv(in_channels, out_channels, cached=False)
+        self.conv_mu = GCNConv(in_channels, out_channels)
+        self.conv_logstd = GCNConv(in_channels, out_channels)
 
     def forward(self, x, edge_index):
         return self.conv_mu(x, edge_index), self.conv_logstd(x, edge_index)
